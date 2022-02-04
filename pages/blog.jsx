328e92332374c40854f9b4c0aed5b9b1399d0e8b
HangCcz <hangchen1128@gmail.com>
2022-02-03 23:38:24 -0500

    created a page for blog/note purpose

---
+++ pages/blog.jsx
@@ -0,0 +1,11 @@
+export default function Blog() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className=" text-2xl font-bold sm:text-3xl md:w-2/3 md:text-4xl lg:text-5xl">
+          A place to keep my blog and notes
+        </h1>
+      </main>
+    </div>
+  )
+}
