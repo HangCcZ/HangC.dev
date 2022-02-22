aebba5697435d923c1c87cd6c17556d986f6a732
HangCcz <hangchen1128@gmail.com>
2022-02-21 20:36:45 -0500

    added place holder for single blog

---
+++ pages/blog/[slug].jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Container from '../../components/Container'
+export default function Blog() {
+  return (
+    <Container>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <h1 className=" text-2xl font-bold sm:text-3xl md:w-2/3 md:text-4xl lg:text-5xl">
+            A place to store single blog
+          </h1>
+        </main>
+      </div>
+    </Container>
+  )
+}
