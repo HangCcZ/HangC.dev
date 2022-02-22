e70165adebf15906dbfc30eada82a8eabb3ff5ce
HangCcz <hangchen1128@gmail.com>
2022-02-21 20:31:23 -0500

    added NavBar and styles

---
+++ components/NavBar.jsx
@@ -0,0 +1,91 @@
+import { Fragment } from 'react'
+import { Disclosure } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { useRouter } from 'next/router'
+import cn from 'classnames'
+import Link from 'next/link'
+
+function NavItems() {
+  const router = useRouter()
+  const navigation = [
+    { name: 'Home', href: '/', current: router.asPath === '/' },
+    {
+      name: 'Projects',
+      href: '/project',
+      current: router.asPath === '/project',
+    },
+    { name: 'Blog', href: '/blog', current: router.asPath === '/blog' },
+    {
+      name: 'Resources',
+      href: '/resource',
+      current: router.asPath === '/resource',
+    },
+  ]
+  return navigation
+}
+
+export default function NavBar() {
+  return (
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-2xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-800 hover:bg-gray-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="hidden w-full sm:ml-6 sm:block">
+                  <div className="flex justify-between">
+                    {NavItems().map((item) => (
+                      <Link href={item.href}>
+                        <a
+                          key={item.name}
+                          className={cn(
+                            item.current ? 'font-semibold' : 'font-normal',
+                            'text-md rounded-md px-3 py-2  text-gray-800 hover:bg-gray-200'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {NavItems().map((item) => (
+                <Link href={item.href}>
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    className={cn(
+                      item.current ? 'font-semibold' : 'font-normal',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                </Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
