d506805ecf74cf2cf819fc7962f30f5dd575bc81
HangCcz <hangchen1128@gmail.com>
2022-02-26 15:36:47 -0500

    added sorting to projectList based on custom priority

--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -3,9 +3,14 @@ import ProjectCard from './ProjectCard'
 export default function ProjectList({ projectList }) {
   return (
     <div className="grid grid-cols-1 gap-4 md:gap-8 ">
-      {projectList.reverse().map((project) => (
-        <ProjectCard key={project.title} project={project} />
-      ))}
+      {projectList
+        .sort((a, b) => {
+          console.log(a, b)
+          return a.priority - b.priority
+        })
+        .map((project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
     </div>
   )
 }
