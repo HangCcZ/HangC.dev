0590c6830c2a9f354a6f152ace1f6868d876d185
HangCcz <hangchen1128@gmail.com>
2022-02-26 15:36:26 -0500

    fixed key props

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -68,7 +68,7 @@ export default function NavBar() {
       <div className={`sm:hidden ${open ? 'block' : 'hidden'}`}>
         <div className="space-y-1 px-2 pt-2 pb-3">
           {NavItems().map((item) => (
-            <Link href={item.href}>
+            <Link href={item.href} key={item.href}>
               <a
                 key={item.name}
                 as="a"
