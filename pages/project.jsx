f79705738ab1594c617dd79a899e202a3f855566
HangCcz <hangchen1128@gmail.com>
2022-02-26 00:25:36 -0500

    added incremental static generation

--- pages/project.jsx
+++ pages/project.jsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
+import ContentfulApi from '../lib/ContentfulApi'
 import Container from '../components/Container'
 import ProjectList from '../components/ProjectList'
-export default function Project() {
+export default function Project({ projectList }) {
   return (
     <Container>
       <div className="mx-auto max-w-3xl">
-        <ProjectList />
+        <ProjectList projectList={projectList} />
       </div>
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const projectList = await ContentfulApi()
+  return {
+    props: { projectList },
+    revalidate: 10,
+  }
 }
