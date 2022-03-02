51b512b2b84e3eeeb92a40ab738e3a518c780bef
HangCcz <hangchen1128@gmail.com>
2022-03-01 22:46:16 -0500

    changed host logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer() {
       </a>
       <a
         className="flex items-center justify-center "
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        href="https://www.netlify.com/"
         target="_blank"
         rel="noopener noreferrer"
       >
         Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
+        <img src="/netlify.svg" alt="Netlify Logo" className="ml-2 h-4" />
       </a>
     </footer>
   )
